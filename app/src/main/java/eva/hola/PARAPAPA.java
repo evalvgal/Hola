503f3a309b48f010c47ba138136dabf397899651
joanm11 <joan.mingorance@e-campus.uab.cat>
2016-03-16 16:46:34 +0100

    j

--- app/src/main/java/eva/hola/PARAPAPA.java
+++ app/src/main/java/eva/hola/PARAPAPA.java
@@ -8,5 +8,6 @@ public class PARAPAPA {
     private String aaaa;
     private String eva;
     private String Manolo;
+    private String Joan;
     boolean estoyAlgoPerdido = true; //Con tiempo todo se consigue xD
 }
